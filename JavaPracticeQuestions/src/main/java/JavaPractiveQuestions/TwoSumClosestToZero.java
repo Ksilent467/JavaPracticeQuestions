7154af3592754b7f54e13e6d2abfae2b107e6abf
HELLO <ksilent467@gmail.com>
2024-10-20 20:03:34 +0530

    TwoSumClosestToZero Code added commit

---
+++ JavaPracticeQuestions/src/main/java/JavaPractiveQuestions/TwoSumClosestToZero.java
@@ -0,0 +1,39 @@
+package JavaPractiveQuestions;
+
+import java.util.Arrays;
+
+public class TwoSumClosestToZero {
+    public static void main(String []args) {
+        int[] arr = {-80, -10, 1, 15, 60, 70, 75};
+        Arrays.sort(arr);
+
+        int start=0;
+        int end=arr.length-1;
+        int curr=0;
+        int res=Integer.MAX_VALUE;
+
+        while(start<end)
+        {
+            curr=arr[start]+arr[end];
+            if(curr==0)
+            {
+                res=curr;
+                break;
+            }
+            else
+            {
+                if(Math.abs(curr)<Math.abs(res) || Math.abs(curr)==Math.abs(res) && curr>res)
+                {
+                    res=curr;
+                }
+                if(curr<0)
+                    start++;
+                else
+                    end--;
+
+            }
+        }
+        System.out.println(res);
+
+    }
+}
