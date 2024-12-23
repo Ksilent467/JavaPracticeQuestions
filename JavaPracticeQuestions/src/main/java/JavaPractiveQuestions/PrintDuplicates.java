90babb77c9231098832ba6763a2ef9eb27760792
HELLO <ksilent467@gmail.com>
2024-12-23 18:43:32 +0530

    New codes added

--- JavaPracticeQuestions/src/main/java/JavaPractiveQuestions/PrintDuplicates.java
+++ JavaPracticeQuestions/src/main/java/JavaPractiveQuestions/PrintDuplicates.java
@@ -1,4 +1,33 @@
 package JavaPractiveQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrintDuplicates {
+
+    public static void main(String args[]){
+        String str="abbdgsafsdaish";
+
+        Map<Character,Integer> map=new HashMap<Character,Integer>();
+        char arr[]=str.toCharArray();
+        for(char ch : arr)
+        {
+            if(!map.containsKey(ch))
+                map.put(ch,1);
+            else
+            {
+                int count = map.get(ch);
+                count++;
+                map.put(ch,count);
+            }
+        }
+       // System.out.println(map);
+
+        for(Map.Entry<Character,Integer> e : map.entrySet())
+        {
+            if(e.getValue()!=1)
+                System.out.println(e.getKey());
+        }
+    }
+
 }
